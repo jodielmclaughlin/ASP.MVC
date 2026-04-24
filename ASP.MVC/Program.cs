a3ec14d344e47958c7187b7ed617eebb5709564b
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-04-24 11:46:02 +0100

    Add project files.

---
+++ ASP.MVC/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+namespace ASP.MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                _ = endpoints.MapControllers();
+            });
+
+            app.Run();
+        }
+    }
+}
